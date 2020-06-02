944b924636f2b128bbf1bac47846f241b25f366e
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2020-06-02 22:17:18 +0200

    Deletion of folder

--- 03-frontend/src/App.js
+++ 03-frontend/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          HEJ2! I was changed on the Feature branch
+          HEJ3! I was changed on the Feature branch
         </p>
       </div>
     );
